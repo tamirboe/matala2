c8543c4d392a37324da4252ea3438492a53e7df8
tamirboe <tamirboe1@gmail.com>
2021-03-31 14:38:22 +0200

    update matala 2

--- text.py
+++ text.py
@@ -5,26 +5,25 @@ Created on Sun Mar 28 08:43:20 2021
 @author: Tamir
 """
 
-def revword(word:str) ->str:
+def revword(word:str) -> str:
     i=len(word)
+    n=""
     for i in word:
         i=i-1
-        new=i+word[i]
+        n=n+word[i]
         
-    return new.lower()
+    return n.lower()
        
-
-def countword() ->int:
+def countword()->int:
     file= open('text.txt')
-    first_line = file.readline()
-    count=0
+    firstline = file.readline()
+    count=1
     for line in file:
         line=line.rsplit()
         for word in line:
-            word= revword(word)
-            if  word in first_line:
+            word=revword(word)
+            if  word in firstline:
                 count= count+1
-    return(count)
-
+    return count
+print(countword())   
    
-      
