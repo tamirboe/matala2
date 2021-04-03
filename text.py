56a24a4c65c03233c3b9c291e7163959505398df
tamirboe <79935267+tamirboe@users.noreply.github.com>
2021-04-03 17:02:01 +0300

    Update text.py

--- text.py
+++ text.py
@@ -1,29 +1,27 @@
-# -*- coding: utf-8 -*-
+-- coding: utf-8 --
 """
 Created on Sun Mar 28 08:43:20 2021
 
 @author: Tamir
 """
 
-def revword(word:str) -> str:
-    i=len(word)
-    n=""
-    for i in range(len(word)):
-        i=i-1
-        n=n+word[i]
-        
+
+def revword(word: str) -> str:
+    n = ""
+    for i in range(len(word) - 1, -1, -1):
+        n = n + word[i]
+
     return n.lower()
-       
-def countword()->int:
-    file= open('text.txt')
-    firstline = file.readline()
-    count=1
+
+
+def countword() -> int:
+    file = open('text.txt')
+    firstline = file.readline().replace("\n", "")
+    count = 1
     for line in file:
-        line=line.rsplit()
+        line = line.rsplit()
         for word in line:
-            word=revword(word)
-            if  word in firstline:
-                count= count+1
+            word = revword(word)
+            if word._eq_(firstline):
+                count = count + 1
     return count
-print(countword())   
-   
